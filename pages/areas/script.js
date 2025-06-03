c46683fe2e0bc695afcbeabc162a59284ea80262
MiguelOFCzzzz <mp254914@gmail.com>
2025-06-03 17:15:56 -0300

    coomit 03. 2.0

--- pages/areas/script.js
+++ pages/areas/script.js
@@ -1,13 +1,32 @@
-document.addEventListener('DOMContentLoaded',async function() {
-    const area = "Mistieri";
-    const token = localStorage.getItem('token');
-    const response = await fetch(`http://localhost:3000/areas/${area}`, {
+document.addEventListener('DOMContentLoaded', async function() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const area = urlParams.get('area');
+    const token = String(localStorage.getItem('token').replaceAll('"', ''));
+    console.log(area);
+    const response = await fetch(`http://localhost:3000/brinquedos/area/${area}`, {
         method: 'GET',
         headers: {    
             'Content-Type': 'application/json',
-            'Authorization': `Bearer` + token
+            'Authorization': `Bearer ` + token
         },
     });
     const data = await response.json();
-    console.log(data);
+    console.log(data.Brinquedos);
+
+    for (var brinquedo of data.Brinquedos) {
+        console.log(brinquedo);
+
+       const grid = document.querySelector('.rides-grid');
+       grid.innerHTML = grid.innerHTML +
+        `
+        <div class="ride-card">
+            <div class="ride-image" style="background-image: url('../dashboard/img/teatro.png')"></div>
+            <div class="ride-info">
+                <h3 class="ride-name">${brinquedo.name}</h3>
+                <div class="ride-time"> ${brinquedo.waiting_time} min</div>
+                <span class="ride-status status-open"> ${brinquedo.status}</span>
+            </div>
+        </div>
+    `
+    }
 });
