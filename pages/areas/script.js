bd7e9f46436e5ea428d1cf2c1b56f7abff30238e
MiguelOFCzzzz <mp254914@gmail.com>
2025-06-10 11:07:36 -0300

    commit 10/06

--- pages/areas/script.js
+++ pages/areas/script.js
@@ -20,7 +20,7 @@ document.addEventListener('DOMContentLoaded', async function() {
        grid.innerHTML = grid.innerHTML +
         `
         <div class="ride-card">
-            <div class="ride-image" style="background-image: url('../dashboard/img/teatro.png')"></div>
+            <div class="ride-image" style="background-image: url('/../../pages/dashboard/img/${brinquedo.image}')"></div>
             <div class="ride-info">
                 <h3 class="ride-name">${brinquedo.name}</h3>
                 <div class="ride-time"> ${brinquedo.waiting_time} min</div>
